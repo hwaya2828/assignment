379933d51b531171867fa0d96c2292aaeda19976
hwaya2828 <hwaya2828@gmail.com>
2021-04-28 23:32:59 +0900

    CREATE: POST&GET

--- owners/views.py
+++ owners/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+import json
+
+from django.http     import JsonResponse
+from django.views    import View
+from owners.models import Owner, Dog
 
 # Create your views here.
+
+class OwnersView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Owner.objects.create(
+            name = data['name'],
+            email = data['email'],
+            age = data['age']
+        )
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+    def get(self, request):
+        owners = Owner.objects.all()
+        results = []
+        for owner in owners:
+            dogs = owner.dog_set.all()
+            total_dog = []
+            for dog in dogs:
+                total_dog.append(
+                    {
+                    "dog's name" : dog.name,
+                    "dog's age" : dog.age
+                    }
+                )
+            results.append(
+                {
+                    "name" : owner.name,
+                    "email" : owner.email,
+                    "age" : owner.age,
+                    "dog(s)" : total_dog
+                }
+        )
+        return JsonResponse({'resutls':results}, status=200)
+
+
+class DogsView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        dog = Dog.objects.create(
+            owner = Owner.objects.get(name=data['owner']),
+            name = data['name'],
+            age = data['age']
+        )
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+    def get(self, request):
+        dogs = Dog.objects.all()
+        results  = []
+        for dog in dogs:
+            results.append(
+                {
+                    "owner" : dog.owner.name,
+                    "name" : dog.name,
+                    "age" : dog.age
+                }
+        )
+        return JsonResponse({'resutls':results}, status=200)
